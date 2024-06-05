7dc1642ba90b796a69fdc2dbef6088152c5ccf3f
Dhruv Sharma <sharmadhruv2511@gmail.com>
2024-06-05 10:27:56 +0530

    Create Account API integration

--- lib/screens/login/bank_details.dart
+++ lib/screens/login/bank_details.dart
@@ -1,9 +1,12 @@
+import 'dart:developer';
 import 'dart:io';
-
+import 'dart:convert';
 import 'package:easy_go_partner/screens/login/otp_screen.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 
 import '../../controller/auth_controller.dart';
 import '../../controller/driver_controller.dart';
@@ -23,28 +26,73 @@ class _BankDetailsState extends State<BankDetails> {
   AuthController authController = Get.put(AuthController());
   DriverController driverController = Get.put(DriverController());
 
-  validate() async {
+  Future<void> createAccount() async {
+    try {
+      final url =
+          Uri.parse('https://easygoapi-eieu6qudpq-uc.a.run.app/accounts');
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          "beneficiary_name": driverController.bNameController.text,
+          "email": driverController.emailController.text,
+          "bank_account_number": driverController.accNumController.text,
+          "ifsc_code": driverController.ifscController.text,
+        }),
+      );
+
+      log('Response status: ${response.statusCode}');
+      log('Response body: ${response.body}');
+
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+        final accountId = responseData['account_id'];
+        await storeAccountIdInFirebase(accountId);
+        routePage();
+      } else {
+        authController
+            .validSnackBar("Failed to create account: ${response.body}");
+      }
+    } catch (e) {
+      authController.validSnackBar("An error occurred: $e");
+    }   
+  }
+
+  Future<void> storeAccountIdInFirebase(String accountId) async {
+    try {
+      final DatabaseReference database = FirebaseDatabase.instance.ref();
+      await database.child('drivers').update({
+        'account_id': accountId,
+      });
+      log('Data added to Realtime Database');
+    } catch (e) {
+      authController
+          .validSnackBar("Failed to store account ID in Firebase: $e");
+    }
+  }
+
+  void validate() async {
     if (driverController.panController.text.isEmpty) {
-      authController.validSnackBar("PAN Number is not empty");
+      authController.validSnackBar("PAN Number cannot be empty");
     } else if (driverController.bNameController.text.isEmpty) {
-      authController.validSnackBar("Bank Name is not empty");
+      authController.validSnackBar("Bank Name cannot be empty");
     } else if (driverController.ifscController.text.isEmpty) {
-      authController.validSnackBar("IFSC Number is not empty");
+      authController.validSnackBar("IFSC Number cannot be empty");
     } else if (driverController.accNumController.text.isEmpty) {
-      authController.validSnackBar("Account Number is not empty");
+      authController.validSnackBar("Account Number cannot be empty");
     } else if (driverController.cAccNumController.text.isEmpty) {
-      authController.validSnackBar("Account Number is not empty");
+      authController.validSnackBar("Re-enter Account Number");
     } else if (driverController.accNumController.text !=
         driverController.cAccNumController.text) {
       authController.validSnackBar("Account numbers do not match");
     } else if (driverController.passBookImg == null) {
       authController.validSnackBar("Upload Passbook image");
     } else {
-      routePage();
+      await createAccount();
     }
   }
 
-  routePage() async {
+  void routePage() async {
     showDialog(
       context: context,
       barrierDismissible: false,
@@ -52,8 +100,7 @@ class _BankDetailsState extends State<BankDetails> {
         return ProgressDialog(message: "Processing, Please wait...");
       },
     );
-    // await driverController.registerUser(widget.phoneNumber);
-    await Future.delayed(Duration());
+    await Future.delayed(Duration(seconds: 2));
     Navigator.pop(context);
     await Get.to(() => OtpScreen(widget.countryCode + widget.phoneNumber));
   }
@@ -106,15 +153,12 @@ class _BankDetailsState extends State<BankDetails> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Register as a Driver",
-          // style: TextStyle(color: Colors.black),
-        ),
+        title: Text("Register as a Driver"),
         backgroundColor: const Color(0xFF0000FF),
         elevation: 0,
       ),
       body: Container(
-        padding: EdgeInsets.only(right: 20.0, left: 20.0),
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
         child: ListView(
           children: [
             const SizedBox(height: 10),
@@ -134,6 +178,12 @@ class _BankDetailsState extends State<BankDetails> {
               controller: driverController.bNameController,
               "Enter Your Bank Name",
               Icons.account_balance_outlined,
+            ),
+            const SizedBox(height: 20),
+            formField(
+              controller: driverController.emailController,
+              "Email",
+              Icons.email_outlined,
             ),
             const SizedBox(height: 20),
             formField(
@@ -220,16 +270,8 @@ class _BankDetailsState extends State<BankDetails> {
               child: CustomButton(
                 hint: "Continue",
                 onPress: () {
+                  log('button click');
                   validate();
-                  // driverController
-                  //     .registerUser(widget.countryCode + widget.phoneNumber);
-                  // Get.to(
-                  //   () => OtpScreen(
-                  //     widget.countryCode + widget.phoneNumber,
-                  //     driverController.nameController.text.trim(),
-                  //     driverController.emailController.text.trim(),
-                  //   ),
-                  // );
                 },
                 borderRadius: BorderRadius.circular(25.0),
                 color: const Color(0xFF0000FF),
--- lib/screens/login/num_screen.dart
+++ lib/screens/login/num_screen.dart
@@ -66,188 +66,190 @@ class _NumberScreenState extends State<NumberScreen> {
                     vertical: constraints.maxHeight * 0.05,
                     horizontal: constraints.maxWidth * 0.05,
                   ),
-                  child: Column(
-                    // mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: constraints.maxHeight * 0.4,
-                        width: constraints.maxWidth * 0.8,
-                        child: Image.asset(
-                          'assets/images/number.png',
-                          fit: BoxFit.contain,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      // mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: constraints.maxHeight * 0.4,
+                          width: constraints.maxWidth * 0.8,
+                          child: Image.asset(
+                            'assets/images/number.png',
+                            fit: BoxFit.contain,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: constraints.maxHeight * 0.01),
-                      Text(
-                        "Let's get started",
-                        style: TextStyle(
-                          fontSize: constraints.maxWidth * 0.055,
-                          fontWeight: FontWeight.bold,
+                        SizedBox(height: constraints.maxHeight * 0.01),
+                        Text(
+                          "Let's get started",
+                          style: TextStyle(
+                            fontSize: constraints.maxWidth * 0.055,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: constraints.maxHeight * 0.01),
-                      Text(
-                        "Enter your valid phone number. We'll send you a verification code",
-                        style: TextStyle(
-                          fontSize: constraints.maxWidth * 0.04,
-                          color: Colors.black45,
-                          // fontWeight: FontWeight.bold,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: constraints.maxHeight * 0.022),
-                      TextFormField(
-                        cursorColor: Colors.blue,
-                        controller: phoneController,
-                        // maxLength: 10,
-                        keyboardType: TextInputType.number,
-                        onChanged: (value) {
-                          setState(() {
-                            phoneController.text = value;
-                          });
-                        },
-                        style: TextStyle(
-                          fontSize: constraints.maxWidth * 0.04,
-                        ),
-                        decoration: InputDecoration(
-                          hintText: "Enter Phone Number",
-                          hintStyle: TextStyle(
-                            fontWeight: FontWeight.w500,
+                        SizedBox(height: constraints.maxHeight * 0.01),
+                        Text(
+                          "Enter your valid phone number. We'll send you a verification code",
+                          style: TextStyle(
                             fontSize: constraints.maxWidth * 0.04,
-                            color: Colors.grey.shade600,
+                            color: Colors.black45,
+                            // fontWeight: FontWeight.bold,
                           ),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(6),
-                            borderSide: const BorderSide(
-                              color: Colors.black26,
-                            ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: constraints.maxHeight * 0.022),
+                        TextFormField(
+                          cursorColor: Colors.blue,
+                          controller: phoneController,
+                          // maxLength: 10,
+                          keyboardType: TextInputType.number,
+                          onChanged: (value) {
+                            setState(() {
+                              phoneController.text = value;
+                            });
+                          },
+                          style: TextStyle(
+                            fontSize: constraints.maxWidth * 0.04,
                           ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(6),
-                            borderSide: const BorderSide(
-                              color: Colors.black26,
+                          decoration: InputDecoration(
+                            hintText: "Enter Phone Number",
+                            hintStyle: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: constraints.maxWidth * 0.04,
+                              color: Colors.grey.shade600,
                             ),
-                          ),
-                          prefixIcon: Container(
-                            padding: const EdgeInsets.only(
-                              top: 13.0,
-                              left: 8,
-                              right: 5,
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(6),
+                              borderSide: const BorderSide(
+                                color: Colors.black26,
+                              ),
                             ),
-                            child: InkWell(
-                              onTap: () {
-                                // showCountryPicker(
-                                //   context: context,
-                                //   countryListTheme:
-                                //       const CountryListThemeData(
-                                //     bottomSheetHeight: 400,
-                                //   ),
-                                //   onSelect: (value) => {
-                                //     setState(() {
-                                //       selectedCountry = value;
-                                //     })
-                                //   },
-                                // );
-                              },
-                              child: Text(
-                                "${selectedCountry.flagEmoji} +${selectedCountry.phoneCode}",
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(6),
+                              borderSide: const BorderSide(
+                                color: Colors.black26,
+                              ),
+                            ),
+                            prefixIcon: Container(
+                              padding: const EdgeInsets.only(
+                                top: 13.0,
+                                left: 8,
+                                right: 5,
+                              ),
+                              child: InkWell(
+                                onTap: () {
+                                  // showCountryPicker(
+                                  //   context: context,
+                                  //   countryListTheme:
+                                  //       const CountryListThemeData(
+                                  //     bottomSheetHeight: 400,
+                                  //   ),
+                                  //   onSelect: (value) => {
+                                  //     setState(() {
+                                  //       selectedCountry = value;
+                                  //     })
+                                  //   },
+                                  // );
+                                },
+                                child: Text(
+                                  "${selectedCountry.flagEmoji} +${selectedCountry.phoneCode}",
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
                               ),
                             ),
+                            suffixIcon: phoneController.text.length.isEqual(10)
+                                ? Container(
+                                    height: 28,
+                                    width: 28,
+                                    margin: const EdgeInsets.all(10.0),
+                                    decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.green,
+                                    ),
+                                    child: const Icon(
+                                      Icons.done,
+                                      color: Colors.white,
+                                      size: 20,
+                                    ),
+                                  )
+                                : null,
                           ),
-                          suffixIcon: phoneController.text.length.isEqual(10)
-                              ? Container(
-                                  height: 28,
-                                  width: 28,
-                                  margin: const EdgeInsets.all(10.0),
-                                  decoration: const BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: Colors.green,
-                                  ),
-                                  child: const Icon(
-                                    Icons.done,
-                                    color: Colors.white,
-                                    size: 20,
-                                  ),
-                                )
-                              : null,
                         ),
-                      ),
-                      SizedBox(height: constraints.maxHeight * 0.02),
-                      SizedBox(
-                        width: double.infinity,
-                        height: constraints.maxHeight * 0.065,
-                        child: CustomButton(
-                          hint: "Login",
-                          color: phoneController.text.length.isEqual(10)
-                              ? const Color(0xFF0000FF)
-                              : Colors.grey,
-                          borderRadius: BorderRadius.circular(25.0),
-                          onPress: phoneController.text.length.isEqual(10)
-                              ? () async {
-                                  try {
-                                    await Future.delayed(
-                                        const Duration(seconds: 5));
-                                    // if (!authController.isValidPhoneNumber(
-                                    //     phoneController.text)) {
-                                    //   // Show an error Snack bar if the phone number is invalid
-                                    //   authController.errorSnackBarNew(
-                                    //       "Invalid phone number. Please enter a valid 10-digit number.");
-                                    //   return;
-                                    // }
-                                    // Proceed with login
-
-                                    // print(
-                                    //     "+${selectedCountry.phoneCode + phoneController.text.trim()}");
-                                    checkUserDataAndNavigate();
-                                    // Get.to(() => LoginOtp(
-                                    //     "+${selectedCountry.phoneCode}" +
-                                    //         phoneController.text.trim()));
-                                  } catch (e) {
-                                    authController.validSnackBar(
-                                        "Error validating phone number: $e");
+                        SizedBox(height: constraints.maxHeight * 0.02),
+                        SizedBox(
+                          width: double.infinity,
+                          height: constraints.maxHeight * 0.065,
+                          child: CustomButton(
+                            hint: "Login",
+                            color: phoneController.text.length.isEqual(10)
+                                ? const Color(0xFF0000FF)
+                                : Colors.grey,
+                            borderRadius: BorderRadius.circular(25.0),
+                            onPress: phoneController.text.length.isEqual(10)
+                                ? () async {
+                                    try {
+                                      await Future.delayed(
+                                          const Duration(seconds: 5));
+                                      // if (!authController.isValidPhoneNumber(
+                                      //     phoneController.text)) {
+                                      //   // Show an error Snack bar if the phone number is invalid
+                                      //   authController.errorSnackBarNew(
+                                      //       "Invalid phone number. Please enter a valid 10-digit number.");
+                                      //   return;
+                                      // }
+                                      // Proceed with login
+                    
+                                      // print(
+                                      //     "+${selectedCountry.phoneCode + phoneController.text.trim()}");
+                                      checkUserDataAndNavigate();
+                                      // Get.to(() => LoginOtp(
+                                      //     "+${selectedCountry.phoneCode}" +
+                                      //         phoneController.text.trim()));
+                                    } catch (e) {
+                                      authController.validSnackBar(
+                                          "Error validating phone number: $e");
+                                    }
                                   }
-                                }
-                              : () {},
-                        ),
-                      ),
-                      SizedBox(height: constraints.maxHeight * 0.035),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: constraints.maxWidth * 0.05),
-                        child: RichText(
-                          textAlign: TextAlign.center,
-                          text: TextSpan(
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: constraints.maxWidth * 0.035),
-                            children: const [
-                              TextSpan(
-                                  text:
-                                      "By creating an account, you agree to our"
-                                      " "),
-                              TextSpan(
-                                text: "Terms of Service ",
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              TextSpan(
-                                text: "and ",
-                                // style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              TextSpan(
-                                text: "Privacy Policy",
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                            ],
+                                : () {},
                           ),
                         ),
-                      ),
-                    ],
+                        SizedBox(height: constraints.maxHeight * 0.035),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: constraints.maxWidth * 0.05),
+                          child: RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: constraints.maxWidth * 0.035),
+                              children: const [
+                                TextSpan(
+                                    text:
+                                        "By creating an account, you agree to our"
+                                        " "),
+                                TextSpan(
+                                  text: "Terms of Service ",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                TextSpan(
+                                  text: "and ",
+                                  // style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                TextSpan(
+                                  text: "Privacy Policy",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
